35a55ad89f097630f5973b7dbc54da3198b8cfeb
Balazs Szabo <szabo.balazs0221@gmail.com>
2018-02-01 13:56:22 +0100

    implemented basic class methods

--- blackjack.py
+++ blackjack.py
@@ -1,30 +1,85 @@
-#This is a text based BlackJack game! Have fun!!
-
-#Global variables
+# This is a text based BlackJack game! Have fun!!
 
 
-#Create classes
+import random
+
+# Global variables
+spades = "\u2660"
+hearts = "\u2665"
+diamonds = "\u2666"
+clubs = "\u2660"
+suits = (spades, hearts, diamonds, clubs)
+ranks = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+values = {'A': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10}
+
+
+# Create classes
 
 class Card():
-    pass
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return str(self.suit) + str(self.rank)
+
 
 class Deck():
-    pass
+
+    def __init__(self):
+        self.cards = []
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(suit, rank))
+
+    def shuffle_deck(self):
+        random.shuffle(self.cards)
+
+    def __str__(self):
+        decklist = ""
+        for i in self.cards:
+            decklist += ' ' + i.__str__()
+        return decklist
+
 
 class Hand():
-    pass
 
-#Other methods
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.ace = False
+
+    def add_card(self, card):
+        self.cards.append(card)
+        if card.rank == 'A':
+            self.ace = True
+        self.value += values[card.rank]
+
+    def __str__(self):
+        current_hand = []
+        for card in self.cards:
+            current_hand += ' ' + card.__str__()
+        return current_hand
+
+
+# Other methods
 
 def text():
     pass
 
-def setupgame():
+
+def setup_game():
     pass
 
-def playblackjack():
+
+def play_blackjack():
     pass
 
-#main function
-if __name__=="__main__":
-    pass
+
+# main function
+if __name__ == "__main__":
+    d = Deck()
+    d.shuffle_deck()
+    print(d)
+    h = Hand()
