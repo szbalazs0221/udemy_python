abc18763a541757ae1842bdd8a6c3e572e6b871b
Balazs Szabo <szabo.balazs0221@gmail.com>
2018-02-01 09:52:27 +0100

    created basic skeleton

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,30 @@
 #This is a text based BlackJack game! Have fun!!
+
+#Global variables
+
+
+#Create classes
+
+class Card():
+    pass
+
+class Deck():
+    pass
+
+class Hand():
+    pass
+
+#Other methods
+
+def text():
+    pass
+
+def setupgame():
+    pass
+
+def playblackjack():
+    pass
+
+#main function
+if __name__=="__main__":
+    pass
